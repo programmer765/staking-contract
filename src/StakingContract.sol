a28fdc95e2ae849a57bdd7cb35770457ffa96bef
Aviprit Shah <avipritshah15@gmail.com>
2026-03-25 20:06:28 +0530

    completed staking contract and its test

--- src/StakingContract.sol
+++ src/StakingContract.sol
@@ -12,39 +12,66 @@ contract StakingContract {
   address private owner;
   address private orcaCoinAddress;
   uint256 private totalStaked;
-  mapping(address => uint256) public stakes;
+  uint256 private rewardRate = 100; // Example reward rate, can be adjusted
+
+  struct UserInfo {
+    uint256 amountStaked;
+    uint256 rewardDebt;
+    uint256 lastUpdateTime;
+  }
+
+  mapping(address => UserInfo) public userInfo;
 
   constructor(address _orcaCoinAddress) {
     owner = msg.sender;
     orcaCoinAddress = _orcaCoinAddress;
   }
 
+  function _updateRewards(address user) internal {
+    uint256 lastUpdate = userInfo[user].lastUpdateTime;
+    userInfo[user].lastUpdateTime = block.timestamp;
+    if (userInfo[user].amountStaked == 0) {
+      return;
+    }
+    uint256 timeDiff = block.timestamp - lastUpdate;
+    uint256 rewards = (userInfo[user].amountStaked * rewardRate * timeDiff) / 1e18;
+    userInfo[user].rewardDebt += rewards;
+  }
+
   function stake() public payable {
     uint256 amt = msg.value;
     require(amt > 0, "Please send a valid amount");
     // uint amtToStake = amt / 1e18;
+    _updateRewards(msg.sender);
     totalStaked += amt;
-    stakes[msg.sender] += amt;
+    userInfo[msg.sender].amountStaked += amt;
   }
 
   function unstake(uint amt) public {
-    require(stakes[msg.sender] >= amt, "Not enough staked");
+    require(userInfo[msg.sender].amountStaked >= amt, "Not enough staked");
     (bool success, ) = payable(msg.sender).call{value: amt}("");
     require(success, "Transfer failed");
+    _updateRewards(msg.sender);
     totalStaked -= amt;
-    stakes[msg.sender] -= amt;
+    userInfo[msg.sender].amountStaked -= amt;
   }
 
-  function getRewards() public {
-
+  function getRewards() public view returns (uint256) {
+    uint256 timeDiff = block.timestamp - userInfo[msg.sender].lastUpdateTime;
+    uint256 rewards = (userInfo[msg.sender].amountStaked * rewardRate * timeDiff) / 1e18;
+    return rewards;
   }
 
   function claimRewards() public {
-
+    _updateRewards(msg.sender);
+    uint256 rewardsToClaim = userInfo[msg.sender].rewardDebt;
+    require(rewardsToClaim > 0, "No rewards to claim");
+    IOrcaCoin(orcaCoinAddress).mint(msg.sender, rewardsToClaim);
+    userInfo[msg.sender].rewardDebt = 0;
   }
 
   function getBalance(address _address) public view returns (uint256) {
-    return stakes[_address];
+    return userInfo[_address].amountStaked;
   }
 
 }
--- test/StakingContract.t.sol
+++ test/StakingContract.t.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/StakingContract.sol";
+import "../src/OrcaCoin.sol";
 
 contract StakingContractTest is Test {
   StakingContract public stakingContract;
+  OrcaCoin public orcaCoin;
 
   receive() external payable {}
 
   function setUp() public {
-    stakingContract = new StakingContract(address(this));
+    orcaCoin = new OrcaCoin(address(this));
+    stakingContract = new StakingContract(address(orcaCoin));
+    orcaCoin.updateStakeContractAddress(address(stakingContract));
   }
 
   function testStake() public {
@@ -31,5 +35,38 @@ contract StakingContractTest is Test {
     stakingContract.stake{value: 1 ether}();
     vm.expectRevert("Not enough staked");
     stakingContract.unstake(2 ether);
+  }
+
+  function testGetRewards() public {
+    vm.deal(address(this), 10 ether);
+    stakingContract.stake{value: 1 ether}();
+    // Simulate time passing
+    vm.warp(block.timestamp + 1 days);
+    uint256 rewards = stakingContract.getRewards();
+    // console.log("Rewards after 1 day:", rewards);
+    assert(rewards > 0);
+  }
+
+  function testClaimRewards() public {
+    vm.deal(address(this), 10 ether);
+    stakingContract.stake{value: 1 ether}();
+    // Simulate time passing
+    vm.warp(block.timestamp + 1 days);
+    stakingContract.claimRewards();
+    // Assuming the mint function works correctly, we would check the balance of OrcaCoin here
+    // For this test, we will just check that rewards have been reset to 0
+    uint256 rewardsAfterClaim = stakingContract.getRewards();
+    assert(rewardsAfterClaim == 0);
+  }
+
+  function test_Revert_IF_ClaimRewards() public {
+    vm.deal(address(this), 10 ether);
+    stakingContract.stake{value: 1 ether}();
+    // Simulate time passing
+    vm.warp(block.timestamp + 1 days);
+    stakingContract.claimRewards();
+    // Attempt to claim rewards again immediately, should revert since rewards are reset
+    vm.expectRevert("No rewards to claim");
+    stakingContract.claimRewards();
   }
 }
